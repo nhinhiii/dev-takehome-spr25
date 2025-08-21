536d1db52d3a1325d50f8498d6cc58e68ba6f31a
nhinhiii <nhinguyenyen0908@gmail.com>
2025-08-20 21:15:45 -0400

    note: the previous one is for get
    This patch is to update 1 item

--- src/app/api/mock/request/route.ts
+++ src/app/api/mock/request/route.ts
@@ -1,3 +1,4 @@
+import { PAGINATION_PAGE_SIZE } from "@/lib/constants/config";
 import dbConnect from "@/lib/dbConnect";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -41,5 +42,63 @@ export async function PUT(request: NextRequest) {
       { message: "Internal Server Error" },
       { status: 500 }
     );
+  }
+}
+
+export async function GET(request: NextRequest) {
+  try {
+    await dbConnect();
+    const {searchParams} = new URL(request.url);
+    const page = parseInt(searchParams.get("page") || "1");
+
+    const filter: any = {};
+    if (status && status !== "all") {
+      filter.status = status;
+    }
+
+    const skip = (page -1) * PAGINATION_PAGE_SIZE;
+    const totalRecords = await Request.countDocuments(filter);
+    const dbRequests = await Request.find(filter)
+      .sort({reuestCreatedDate: -1})
+      .skip(skip)
+      .limit(PAGINATION_PAGE_SIZE)
+      .lean();
+    
+    const formattedData = dbRequests.map(formatRequest);
+
+    return NextResponse.json({
+      data: formattedData,
+      page,
+      totalRecords,
+      pageSizeL: PAGINATION_PAGE_SIZE,
+    })
+  } catch (error) {
+    console.error("Error fetching requests:", error);
+    return NextResponse.json({message: "Internal Server Error"}, {status: 500});
+  }
+}
+
+export async function PATCH(request: NextRequest) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+    const {id, ids, status} = body;
+
+    if(!status) {
+      return NextResponse.json({message: "Status is required for update."}, {status: 400})
+    }
+
+    if(id) {
+      const updatedRequest = await Request.findByAndUpdate(
+        id,
+        {$set: {status: status, lastEditedDate: new Date()}},
+        {new: true}
+      );
+
+      if(!updatedRequest) {
+        return NextResponse.json({message: `Request with ID ${id} not found`}, {status: 404});
+      }
+      return NextResponse.json(formatRequest(updatedRequest), {status: 200});
+    }
   }
 }

5b236a89e1075c4258af2b5c8cffda2a0c57892c
nhinhiii <nhinguyenyen0908@gmail.com>
2025-08-20 22:54:00 -0400

    add Delete and check typo

--- src/app/api/mock/request/route.ts
+++ src/app/api/mock/request/route.ts
@@ -1,6 +1,8 @@
 import { PAGINATION_PAGE_SIZE } from "@/lib/constants/config";
 import dbConnect from "@/lib/dbConnect";
+import Request from "@/models/Request";
 import { NextRequest, NextResponse } from "next/server";
+import { RequestStatus } from "@/lib/types/request";
 
 function formatRequest(dbRequest: any) {
   return {
@@ -9,8 +11,8 @@ function formatRequest(dbRequest: any) {
     itemRequested: dbRequest.itemRequested,
     status: dbRequest.status,
     requestCreatedDate: dbRequest.requestCreatedDate.toISOString(),
-    lastEditedDated: dbRequest.lastEditedDate
-      ? dbRequest.lastEditedDate.toIOString()
+    lastEditedDate: dbRequest.lastEditedDate
+      ? dbRequest.lastEditedDate.toISOString()
       : null,
   };
 }
@@ -19,7 +21,7 @@ export async function PUT(request: NextRequest) {
   try {
     await dbConnect();
     const { requestorName, itemRequested } = await request.json();
-    if (!requestorName || itemRequested) {
+    if (!requestorName || !itemRequested) {
       return NextResponse.json(
         { message: "Requestor name and item are required." },
         { status: 400 }
@@ -37,7 +39,7 @@ export async function PUT(request: NextRequest) {
     if (error.name === "ValidationError") {
       return NextResponse.json({ message: error.message }, { status: 400 });
     }
-    console.log("Error creating request:", error);
+    console.error("Error creating request:", error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
@@ -50,6 +52,7 @@ export async function GET(request: NextRequest) {
     await dbConnect();
     const { searchParams } = new URL(request.url);
     const page = parseInt(searchParams.get("page") || "1");
+    const status = searchParams.get("status") as RequestStatus | null;
 
     const filter: any = {};
     if (status && status !== "all") {
@@ -59,7 +62,7 @@ export async function GET(request: NextRequest) {
     const skip = (page - 1) * PAGINATION_PAGE_SIZE;
     const totalRecords = await Request.countDocuments(filter);
     const dbRequests = await Request.find(filter)
-      .sort({ reuestCreatedDate: -1 })
+      .sort({ requestCreatedDate: -1 })
       .skip(skip)
       .limit(PAGINATION_PAGE_SIZE)
       .lean();
@@ -70,7 +73,7 @@ export async function GET(request: NextRequest) {
       data: formattedData,
       page,
       totalRecords,
-      pageSizeL: PAGINATION_PAGE_SIZE,
+      pageSize: PAGINATION_PAGE_SIZE,
     });
   } catch (error) {
     console.error("Error fetching requests:", error);
@@ -95,7 +98,7 @@ export async function PATCH(request: NextRequest) {
     }
 
     if (id) {
-      const updatedRequest = await Request.findByAndUpdate(
+      const updatedRequest = await Request.findByIdAndUpdate(
         id,
         { $set: { status: status, lastEditedDate: new Date() } },
         { new: true }
@@ -103,17 +106,17 @@ export async function PATCH(request: NextRequest) {
 
       if (!updatedRequest) {
         return NextResponse.json(
-          { message: `Request with ID ${id} not found` },
+          { message: `Request with ID ${id} not found.` },
           { status: 404 }
         );
       }
       return NextResponse.json(formatRequest(updatedRequest), { status: 200 });
     } else if (ids && Array.isArray(ids) && ids.length > 0) {
-      const updatedResult = await Request.updateMany(
+      const updateResult = await Request.updateMany(
         { _id: { $in: ids } },
         { $set: { status: status, lastEditedDate: new Date() } }
       );
-      if (updatedResult.matchedCount === 0) {
+      if (updateResult.matchedCount === 0) {
         return NextResponse.json(
           { message: "No matching requests found for the given IDs." },
           { status: 404 }
@@ -121,7 +124,7 @@ export async function PATCH(request: NextRequest) {
       }
       return NextResponse.json(
         {
-          message: `${updatedResult.modifiedCount} requests updated successfully`,
+          message: `${updateResult.modifiedCount} requests updated successfully.`,
         },
         { status: 200 }
       );
@@ -142,6 +145,50 @@ export async function PATCH(request: NextRequest) {
       );
     }
     console.error("Error updating request(s):", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    await dbConnect();
+    const { ids } = await request.json();
+
+    if (!ids || !Array.isArray(ids) || ids.length === 0) {
+      return NextResponse.json(
+        { message: "An array of request IDs is required for deletion." },
+        { status: 400 }
+      );
+    }
+
+    const deleteResult = await Request.deleteMany({
+      _id: { $in: ids },
+    });
+
+    if (deleteResult.deletedCount === 0) {
+      return NextResponse.json(
+        {
+          message: "No matching requests found with the given IDs",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: `${deleteResult.deletedCount} requests deleted successfully` },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    if (error.name === "CastError") {
+      return NextResponse.json(
+        { message: `Invalid ID format found in the provided list` },
+        { status: 400 }
+      );
+    }
+    console.error("Error deleting request:", error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
